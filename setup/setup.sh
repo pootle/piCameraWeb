f70b72ae611225b89ca3375ec4d782f75f9971b7
pootle <iainmalcolm1@gmail.com>
2019-02-12 10:20:59 +0000

    minor updates to improve output from setup

--- setup/setup.sh
+++ setup/setup.sh
@@ -1,23 +1,32 @@
 #!/bin/sh
 
 echo "piCameraWeb prep and setup script........"
+echo "======================================"
 echo "checking system is up to date........."
 
 apt-get -q update
 
 apt-get -q -y update
 
+echo "====================================================="
 echo "installing addtional software required by piCameraWeb"
+echo "====================================================="
 
 apt-get -q -y install python3-pip gpac python3-pigpio python3-numpy
 
+echo "===================================="
 echo "installing additional python modules"
+echo "===================================="
 
 pip3 install picamera pypng
 
+echo "===================================="
 echo "applying patches to picamera package"
+echo "===================================="
 
 patch /usr/local/lib/python3.5/dist-packages/picamera/streams.py streams.patch
 patch /usr/local/lib/python3.5/dist-packages/picamera/camera.py camera.patch
 patch /usr/local/lib/python3.5/dist-packages/picamera/encoders.py encoders.patch
 
+echo "=============="
+echo "setup complete"
