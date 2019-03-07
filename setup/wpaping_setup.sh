40376e391dd1d704f2bf502b923a9efa05aaf745
pootle <iainmalcolm1@googlemail.com>
2019-03-07 09:14:05 +0000

    fix typo in wpaping_setup.sh

--- setup/wpaping_setup.sh
+++ setup/wpaping_setup.sh
@@ -4,7 +4,7 @@ if ifconfig | grep -Fq "wlan0"; then
         while true; do
             read -p  "Install wpaping (on raspbain lite stretch this stops wifi bouncing)?" yn
             case $yn in
-                [Yy]* ) cp wpaping.service /lib/systemd/system;systemctl daemon-reload; systemctl enable wpaping.service; systemctl start wpaping.service  break;;
+                [Yy]* ) cp wpaping.service /lib/systemd/system;systemctl daemon-reload; systemctl enable wpaping.service; systemctl start wpaping.service;  break;;
                 [Nn]* ) exit;;
                 * ) echo "Please answer yes or no.";;
             esac
